d71996e265d1008d950155770a585212bc97bca4
Oscar <odrusso1@gmail.com>
2022-11-23 14:14:52 +1300

    fix mobile layout

--- comp-med/app/layout.tsx
+++ comp-med/app/layout.tsx
@@ -4,6 +4,7 @@ import '../styles/globals.css'
 export default function RootLayout({children,}: { children: React.ReactNode }) {
     return (
         <html>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
         <body>{children}</body>
         </html>
     )
